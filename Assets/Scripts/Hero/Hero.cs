4a9650953086ad92509e74fb4ff0b91df700a678
choiwoosik1 <81142069+choiwoosik1@users.noreply.github.com>
2025-06-13 19:50:39 +0900

    Second
    주인공 캐릭터 움직임 관련

--- Assets/Scripts/Hero/Hero.cs
+++ Assets/Scripts/Hero/Hero.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 /// </summary>
 public class Hero : MonoBehaviour
 {
+    [Header("---- 컴포넌트 참조 ----")]
+    [SerializeField] Mover _mover;
+
     /// <summary>
     /// 이동 속력
     /// </summary>
@@ -24,30 +27,6 @@ public class Hero : MonoBehaviour
     /// <param name="direction">이동 방향</param>
     public void Move(Vector3 direction)
     {
-        // y방향 이동은 무시
-        direction.y = 0;
-
-        // 이동 방향이 0이면(없으면) 이동하지 않음
-        if (direction.magnitude < Utils.Epsilon)
-        {
-            return;
-        }
-
-        // 이동
-        transform.Translate(
-            direction.normalized * _moveSpeed * Time.deltaTime, Space.World);
-
-        // 회전
-        // 1) 특정 방향을 즉시 바라보게 설정(transform.forward 활용)
-        //transform.forward = direction;
-
-        // 2) 특정 방향을 천천히 바라보게 설정
-        // LookRotation() : 특정 방향을 바라보는 회전 값을 만들어 주는 함수
-        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-        
-        transform.rotation = Quaternion.RotateTowards(
-            transform.rotation,             // 현재 회전 값
-            targetRotation,                 // 목표 회전 값
-            _rotSpeed * Time.deltaTime );   // 프레임당 회전 정도
+        _mover.Move(direction);
     }
 }
---
+++ Assets/Scripts/Mover/TransformMover.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMover : Mover
+{
+    /// <summary>
+    /// 회전 속력
+    /// </summary>
+    [SerializeField] float _rotSpeed;
+
+    public override event Action<Vector3> OnMoved;
+
+    public override void Move(Vector3 direction)
+    {
+        // y방향 이동은 무시
+        direction.y = 0;
+
+        // 이동 방향이 0이면(없으면) 이동하지 않음
+        if (direction.magnitude < Utils.Epsilon)
+        {
+            return;
+        }
+
+        // 이동
+        transform.Translate(
+            direction.normalized * _moveSpeed * Time.deltaTime, Space.World);
+
+        // 회전
+        // 1) 특정 방향을 즉시 바라보게 설정(transform.forward 활용)
+        //transform.forward = direction;
+
+        // 2) 특정 방향을 천천히 바라보게 설정
+        // LookRotation() : 특정 방향을 바라보는 회전 값을 만들어 주는 함수
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,             // 현재 회전 값
+            targetRotation,                 // 목표 회전 값
+            _rotSpeed * Time.deltaTime);   // 프레임당 회전 정도
+    }
+}
